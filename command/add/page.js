56441f22aaec0ba30f9bbc4926e95480669a67d3
gd4ark <gd4ark@gmail.com>
2019-07-25 12:56:01 +0800

    feat(移除 generator):

--- command/add/page.js
+++ command/add/page.js
@@ -2,101 +2,29 @@ const fs = require('fs')
 const { join } = require('path')
 const { prompt } = require('inquirer')
 const ora = require('ora')
-const upperFirst = require('lodash/upperFirst')
 const { template_path, user_path, template_json_path } = require('../../config')
 const utils = require('../../lib/utils')
 const logger = require('../../lib/utils/logger')
 
-const templteJson = require(template_json_path)
-const pages = templteJson.pages
-const template_name = templteJson.name
+const templteJSON = require(template_json_path)
+const template_name = templteJSON.name
+
 const generator_path = join(template_path, `${template_name}/generator`)
 
-let tmp_page_value = 'admin'
-const question = [
-    {
-        name: 'page',
-        type: 'input',
-        message: '添加到页面',
-        default: tmp_page_value,
-        validate(val) {
-            if (val === '') {
-                return '页面名称为必填'
-            }
-            tmp_page_value = val
-            return true
-        }
-    },
-    {
-        name: 'name',
-        type: 'input',
-        message: '请输入页面名称(英文)',
-        validate(val) {
-            const page = pages.find(
-                item => item.name === val && item.page === tmp_page_value
-            )
-            if (val === '') {
-                return '页面名称为必填'
-            } else if (page) {
-                return '页面名称已存在'
-            } else {
-                return true
-            }
-        }
-    },
-    {
-        name: 'title',
-        type: 'input',
-        message: '请输入页面标题(中文)',
-        validate(val) {
-            if (val === '') {
-                return '页面名称为必填'
-            } else {
-                return true
-            }
-        }
-    }
-]
-
-function getFileTplList(answers) {
-    const { name, page } = answers
-    return [
-        {
-            from: join(generator_path, 'data/tpl.js'),
-            to: join(user_path, `src/common/data/modules/${name}.js`)
-        },
-        {
-            from: join(generator_path, 'router/tpl.js'),
-            to: join(user_path, `src/pages/${page}/router/routers/${name}.js`)
-        },
-        {
-            from: join(generator_path, 'store/tpl.js'),
-            to: join(user_path, `src/pages/${page}/store/modules/${name}.js`)
-        },
-        {
-            from: join(generator_path, 'view/tpl.vue'),
-            to: join(user_path, `src/pages/${page}/views/${name}/index.vue`)
-        },
-        {
-            from: join(generator_path, 'view/components/table.vue'),
-            to: join(
-                user_path,
-                `src/pages/${page}/views/${name}/components/${upperFirst(
-                    name
-                )}Table.vue`
-            )
-        }
-    ]
-}
+const { question, getFileTplList } = require(join(
+    generator_path,
+    'command/add/page.js'
+))
 
 module.exports = async() => {
     const answers = await prompt(question)
-    const files = getFileTplList(answers)
+    const files = getFileTplList(answers, user_path)
+    console.log(answers)
     const spinner = ora('添加页面中...')
     spinner.start()
     await utils.generate(files, answers)
-    templteJson.pages.push(answers)
-    fs.writeFile(template_json_path, JSON.stringify(templteJson), err => {
+    templteJSON.pages.push(answers)
+    fs.writeFile(template_json_path, JSON.stringify(templteJSON), err => {
         if (err) logger.fatal('创建失败！', err)
         spinner.stop()
         logger.success(`添加页面 ${answers.name} 成功`)
--- command/delete/page.js
+++ command/delete/page.js
@@ -2,69 +2,32 @@ const fs = require('fs')
 const { join } = require('path')
 const { prompt } = require('inquirer')
 const ora = require('ora')
-const { user_path, template_json_path } = require('../../config')
+const { template_path, user_path, template_json_path } = require('../../config')
 const utils = require('../../lib/utils')
 const logger = require('../../lib/utils/logger')
 
-const templteJson = require(template_json_path)
+const templteJSON = require(template_json_path)
+const template_name = templteJSON.name
 
-const question = [
-    {
-        name: 'page',
-        type: 'input',
-        message: '属于哪个页面',
-        default: 'admin',
-        validate(val) {
-            if (val === '') {
-                return '页面为必填'
-            }
-            return true
-        }
-    },
-    {
-        name: 'name',
-        type: 'input',
-        message: '请输入页面名称(英文)',
-        validate(val) {
-            if (val === '') {
-                return '页面名称为必填'
-            } else {
-                return true
-            }
-        }
-    }
-]
+const generator_path = join(template_path, `${template_name}/generator`)
 
-function getFileTplList(answers) {
-    const { name, page } = answers
-    return [
-        {
-            path: join(user_path, `src/common/data/modules/${name}.js`)
-        },
-        {
-            path: join(user_path, `src/pages/${page}/router/routers/${name}.js`)
-        },
-        {
-            path: join(user_path, `src/pages/${page}/store/modules/${name}.js`)
-        },
-        {
-            path: join(user_path, `src/pages/${page}/views/${name}`)
-        }
-    ]
-}
+const { question, getFileTplList } = require(join(
+    generator_path,
+    'command/delete/page.js'
+))
 
 module.exports = async() => {
     const answers = await prompt(question)
-    const files = getFileTplList(answers)
+    const files = getFileTplList(answers, user_path)
     const spinner = ora('删除页面中...')
     spinner.start()
     await utils.deleteFiles(files)
-    templteJson.pages = templteJson.pages.filter(item => {
+    templteJSON.pages = templteJSON.pages.filter(item => {
         if (item.name !== answers.name) {
             return item
         }
     })
-    fs.writeFile(template_json_path, JSON.stringify(templteJson), err => {
+    fs.writeFile(template_json_path, JSON.stringify(templteJSON), err => {
         if (err) logger.fatal('删除失败！', err)
         spinner.stop()
         logger.success(`删除页面 ${answers.name} 成功`)
